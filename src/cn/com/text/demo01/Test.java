8bc54c405e52e49871bf3f863b4c5b01c91157a2
wch1000 <75842419@qq.com>
2019-07-03 00:49:27 +0800

    测试2

--- src/cn/com/text/demo01/Test.java
+++ src/cn/com/text/demo01/Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("你好测试Git");
         System.out.println("你好测试Git1");
+        System.out.println("你好测试Git2");
 
 
     }

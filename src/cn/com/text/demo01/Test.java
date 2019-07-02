aeae4ccb0372fdf222e7972987433677e40f678e
wch1000 <75842419@qq.com>
2019-07-03 00:44:37 +0800

    测试1

--- src/cn/com/text/demo01/Test.java
+++ src/cn/com/text/demo01/Test.java
@@ -3,5 +3,8 @@ package cn.com.text.demo01;
 public class Test {
     public static void main(String[] args) {
         System.out.println("你好测试Git");
+        System.out.println("你好测试Git1");
+
+
     }
 }
